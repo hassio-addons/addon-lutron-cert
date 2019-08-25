7d42e0edb8d1042866787bc2c528855be8e3bf33
Franck Nijhof <frenck@addons.community>
2019-08-25 21:40:46 +0200

    :ambulance: Ensure we have a DBus UUID at any time

---
+++ lutron-cert/rootfs/etc/cont-init.d/uuidgen.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: Lutron Certificate
+# Ensure we have a valid UUID to work with
+# ==============================================================================
+dbus-uuidgen --ensure
